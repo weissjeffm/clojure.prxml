cf6c4ef04718fbab8ea3e4dccd059c608c656525
Jeff Weiss <jweiss@redhat.com>
2012-01-17 22:09:35 -0200

    initial commit

---
+++ clojure/prxml/core.clj
@@ -0,0 +1,1 @@
+(ns clojure.prxml.core)
---
+++ project.clj
@@ -0,0 +1,3 @@
+(defproject weissjeffm/clojure.prxml "1.3.0-SNAPSHOT"
+  :description "1.3.0 compatible prxml"
+  :dependencies [[org.clojure/clojure "1.3.0"]])
